94bb9094752d16d1b3439b1e24a065990518d612
shiwidembla <shiwani.bcskkf18@iba-suk.edu.pk>
2023-05-24 21:57:58 +0500

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,21 @@
-import logo from './logo.svg';
-import './App.css';
-
-function App() {
+export default function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+    <div>
+      
+    <nav>
+      <ul className="px-4 py-4 flex bg-purple-900 text-white">
+        <li className="mx-2 cursor-pointer">Home</li>
+        <li className="mx-2 cursor-pointer">About</li>
+        <li className="mx-2 cursor-pointer">Contact</li>
+      </ul>
+    </nav>
+      
+    
+    <h1 className="text-3xl font-bold text-white">
+      Hello world!
+    </h1>
 
-export default App;
+
+    </div>
+  )
+}
